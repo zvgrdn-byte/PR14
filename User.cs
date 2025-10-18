a16ba83c0c0096139b68a027a43e4022e4ecd375
andrey <zvgr@outlook.com>
2025-10-18 12:44:55 +0300

    Добавлен класс User с основными свойствами

---
+++ User.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ControlsPractice.Models
+{
+    public class User : INotifyPropertyChanged
+    {
+        private string? _name;
+        private string? _age;
+        private string? _country;
+        private bool _isSubscribed;
+
+        public string? Name
+        {
+            get => _name;
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+        public string? Age
+        {
+            get => _age;
+            set { _age = value; OnPropertyChanged(); }
+        }
+
+        public string? Country
+        {
+            get => _country;
+            set { _country = value; OnPropertyChanged(); }
+        }
+
+        public bool IsSubscribed
+        {
+            get => _isSubscribed;
+            set { _isSubscribed = value; OnPropertyChanged(); }
+        }
+
+        public string Display => $"{Name}, {Age} лет, {Country}" + (IsSubscribed ? " (подписан)" : "");
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
